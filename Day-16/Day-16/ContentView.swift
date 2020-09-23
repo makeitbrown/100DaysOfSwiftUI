fe2487712040d942f6d86b6328fb4e41d09f6908
Michael Brown <brown.michael.n@gmail.com>
2020-09-23 09:56:42 -0600

    Created and started WeSplit Day 16-17

---
+++ Day-16/Day-16/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Day-16
+//
+//  Created by Michael Brown on 9/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day-16/Day-16/Day_16App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_16App.swift
+//  Day-16
+//
+//  Created by Michael Brown on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Day_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
