e5006fd4d8a34e4c9d590aadd4af8114833c00d7
Michael Brown <brown.michael.n@gmail.com>
2020-10-01 10:34:26 -0600

    GuessTheFlag Project Complete before challenge

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michael Brown on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
